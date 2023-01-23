f259661ba40e8483cf698f55102b2d09a868e532
Kajal lathiya <kajalbodarya77@gmail.com>
2023-01-24 00:37:49 +0530

    profile upload & error handler

---
+++ server/src/errorHandlers.js
@@ -0,0 +1,30 @@
+export const badRequestHander = (err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({ message: err.message, list: err.errorsList });
+  } else {
+    next(err);
+  }
+};
+
+export const unauthorizedHandler = (err, req, res, next) => {
+  if (err.status === 401) {
+    res.status(401).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const notFoundHandler = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ message: err.message });
+  } else {
+    next(err);
+  }
+};
+
+export const genericErrorHandler = (err, req, res, next) => {
+  console.log("ERROR RECEIVED FROM UP ABOVE:", err);
+  res
+    .status(500)
+    .send({ message: "An error occurred on our side! we gonna fix that asap" });
+};
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,12 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 import usersRouter from "./api/usersRouter.js";
+import {
+  badRequestHander,
+  unauthorizedHandler,
+  notFoundHandler,
+  genericErrorHandler
+} from "./errorHandlers.js";
 
 const port = process.env.PORT;
 
@@ -12,13 +18,19 @@ server.use(express.json());
 //**************************** ENDPOINTS **************************** */
 server.use("/users", usersRouter);
 
+//**************************** ERROR HANDLERS **************************** */
+server.use(badRequestHander); // 400
+server.use(unauthorizedHandler); // 401
+server.use(notFoundHandler); // 404
+server.use(genericErrorHandler); // 500
+
 //************************** MONGODB CONNECT ************************** */
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGO_URL);
 mongoose.connection.on("connected", () => {
   console.log("DB:ON Live");
-    server.listen(port, () => {
-        console.table(listEndpoints(server));
+  server.listen(port, () => {
+    console.table(listEndpoints(server));
     console.log("Sever running on port:", port);
   });
 });
